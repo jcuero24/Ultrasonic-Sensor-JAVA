c4f43f1972d3ec4873cc8cf5de5f276eb1824425
jcuero24 <cueroj505@gmail.com>
2025-11-06 13:11:26 -0500

    U.System

---
+++ src/sistemaultrasonico/SistemaUltrasonico.java
@@ -0,0 +1,9 @@
+package sistemaultrasonico;
+
+public class SistemaUltrasonico {
+    public static void main(String[] args) {
+        // Lanza la ventana principal del programa
+        Ventana_Principal ventana = new Ventana_Principal();
+        ventana.setVisible(true);
+    }
+}
